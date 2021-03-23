2a801c5ebbddb2ba69ecc225a55ce1ec31b521f7
kmilic01 <kmilic01@fesb.hr>
2021-03-23 19:48:33 +0100

    Add project files.

---
+++ pis1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
